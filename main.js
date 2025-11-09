c6ad7e623e9131c45372c2b7fd3daba5bebd2d1c
Tamer <tamersya@gmail.com>
2025-11-09 21:36:06 +0500

    update

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navLinks.classList.toggle('active');
 });
+
+
+
+// offers download
+document.getElementById('downloadBtn').addEventListener('click', () => {
+  const link = document.createElement('a');
+  link.href = 'Travel_agency_Report.pdf';
+  link.download = 'Travel_agency_Report.pdf';
+  link.click();
+});
+
+
